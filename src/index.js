6be2c00f9862e38284dc53cab4ce39c009e3f180
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-12 13:57:09 -0500

    Initial commit for file setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const _ = require('lodash');
+const $ = require('jquery');
+
+const sayHello = () => console.log('Hello');
+
+sayHello();
+
+console.log(_.random(0,3));
+
+$('.fancy-header').append(', is');
